9f68bfb8fa0ed7f37027bb0ee76f2faa79b5bad1
Zack DARK <zakaria.janah2018@gmail.com>
2025-11-03 15:50:42 +0100

    Added user profile feed API

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -7,6 +7,17 @@ class UpdateOwnProfile(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         """Check user is trying to edit their own profile"""
         if request.method in permissions.SAFE_METHODS:
-            return True 
+            return True
 
         return obj.id == request.user.id
+
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allow users to update their own status"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check the user is trying to updatetheir own status"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id 
