82eaf2a1b015dd3d959de79ca3c103d88bc5df01
zack-dark <84252730+zack-dark@users.noreply.github.com>
2025-11-04 17:55:01 +0100

    Update setup.sh

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -13,7 +13,7 @@ locale-gen en_GB.UTF-8
 # Install Python, SQLite, pip, supervisor, nginx, git, and distutils
 echo "Installing dependencies..."
 apt-get update
-apt-get install -y python3-dev python3-venv sqlite3 python3-pip supervisor nginx git python3-distutils
+apt-get install -y python3-dev python3-venv sqlite3 python3-pip supervisor nginx git python3
 
 # Create project directory and clone the repository
 mkdir -p $PROJECT_BASE_PATH
